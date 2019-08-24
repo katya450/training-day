b31e1ea7c2f9f6a7aa427ee39531941614d20ee3
Katja Lusma <katja.lusma@siili.com>
2019-08-24 15:40:45 +0300

    solutions

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,20 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [x] (* x x))
 
-(defn average [a b]
-  ":(")
+(defn average [a b] (/ (+ a b) 2 )) ;a+b /2
+
+;(+ 4 (* 2 3))
+
+;(+ 3 4 5 6)
+
+;(get "abrakadabra" 0)
+
+;(fn [who] (str "Hello, " who "!"))
+;((fn [who] (str "Hello, " who "!")) "Jani") ;=> "Hello, Jani!"
+
+;((fn [name] (str "Welcome to Rivendell ms " name)) "Katja")
